18b40e3d8389b1ffd91f4ef8840bee493cce712a
vinikjkkj <viniwarrior4@gmail.com>
2025-01-15 13:06:57 -0300

    use node-addon-api

--- native/ed25519/additions/curve_sigs.c
+++ native/ed25519/additions/curve_sigs.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include "ge.h"
 #include "curve_sigs.h"
 #include "crypto_sign.h"
@@ -39,7 +40,7 @@ void curve25519_sign(unsigned char* signature_out,
 {
   ge_p3 ed_pubkey_point; /* Ed25519 pubkey point */
   unsigned char ed_keypair[64]; /* privkey followed by pubkey */
-  unsigned char sigbuf[msg_len + 64]; /* working buffer */
+  unsigned char* sigbuf = (unsigned char*)malloc(msg_len + 64); /* working buffer */
   unsigned long long sigbuf_out_len = 0;
   unsigned char sign_bit = 0;
 
@@ -54,7 +55,10 @@ void curve25519_sign(unsigned char* signature_out,
   memmove(signature_out, sigbuf, 64);
 
   /* Encode the sign bit into signature (in unused high bit of S) */
-   signature_out[63] |= sign_bit;
+  signature_out[63] &= 0x7F; /* bit should be zero already, but just in case */
+  signature_out[63] |= sign_bit;
+  
+  free(sigbuf);
 }
 
 int curve25519_verify(unsigned char* signature,
@@ -66,8 +70,9 @@ int curve25519_verify(unsigned char* signature,
   fe ed_y;
   unsigned char ed_pubkey[32];
   unsigned long long some_retval;
-  unsigned char verifybuf[msg_len + 64]; /* working buffer */
-  unsigned char verifybuf2[msg_len + 64]; /* working buffer #2 */
+  unsigned char* verifybuf = NULL;
+  unsigned char* verifybuf2 = NULL;
+  int retval;
 
   /* Convert the Curve25519 public key into an Ed25519 public key.  In
      particular, convert Curve25519's "montgomery" x-coordinate into an
@@ -75,7 +80,7 @@ int curve25519_verify(unsigned char* signature,
 
      ed_y = (mont_x - 1) / (mont_x + 1)
 
-     Then move the sign bit into the pubkey from the signature.
+     NOTE: mont_x=-1 is converted to ed_y=0 since fe_invert is mod-exp
   */
   fe_frombytes(mont_x, curve25519_pubkey);
   fe_1(one);
@@ -86,12 +91,22 @@ int curve25519_verify(unsigned char* signature,
   fe_tobytes(ed_pubkey, ed_y);
 
   /* Copy the sign bit, and remove it from signature */
+  ed_pubkey[31] &= 0x7F;  /* bit should be zero already, but just in case */
   ed_pubkey[31] |= (signature[63] & 0x80);
   signature[63] &= 0x7F;
+
+  /* Allocate buffers for verification */
+  verifybuf = (unsigned char*)malloc(msg_len + 64);
+  verifybuf2 = (unsigned char*)malloc(msg_len + 64);
 
   memmove(verifybuf, signature, 64);
   memmove(verifybuf+64, msg, msg_len);
 
-  /* Then perform a normal Ed25519 verification, return 0 on success */
-  return crypto_sign_open(verifybuf2, &some_retval, verifybuf, 64 + msg_len, ed_pubkey);
+  /* Perform an Ed25519 verification */
+  retval = crypto_sign_open(verifybuf2, &some_retval, verifybuf, 64 + msg_len, ed_pubkey);
+
+  free(verifybuf);
+  free(verifybuf2);
+  
+  return retval;
 }
